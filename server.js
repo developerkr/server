402da2eaea1d5ebd8e8fd45dfa0482e4f6f37da9
developerkr <74369441+developerkr@users.noreply.github.com>
2021-03-30 22:09:23 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000  
+const bodyParser = require('body-parser')
+
+app.use(express.static('public'))
+app.use(express.urlencoded())
+app.use(express.json())
+// app.use((req,res,next)=>{
+//     console.log(req)
+//     res.send("hello")
+// }) 
+app.get('/user',(req, res)=>{
+    
+    res.sendFile(__dirname+'\\src\\index.html')
+    console.log(__dirname)
+})
+
+app.post('/user', (req,res)=>{
+    console.log(req.body)
+    res.send('ok')
+})
+
+app.listen(port,(err)=>{
+    console.log(err)
+    console.log(`Example app listening  at https://localhost:${port}`)
+})
+
