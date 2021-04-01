26468b2f28ee1d07c4dae6b73ec2c8cf181690a9
developerkr <74369441+developerkr@users.noreply.github.com>
2021-04-01 15:12:01 +0500

    add style

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ const app = express()
 const port = process.env.PORT||3000  
 const bodyParser = require('body-parser')
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null, './public')
+    },
+    filename:(req,file,cb)=>{
+        cb(null,"MyImage"+Date.now()+ file.originalname)
+    }
+})
+
+const upload = multer({storage})
+
+//middlewares
+
 app.use(express.static('public'))
 app.use(express.urlencoded())
 app.use(express.json())
@@ -17,9 +32,14 @@ app.get('/user',(req, res)=>{
     console.log(__dirname)
 })
 
-app.post('/user', (req,res)=>{
-    console.log(req.body)
-    res.send(`Jo'natildi`)
+app.post('/user', upload.array('picture',100),(req,res)=>{
+    console.log(req.files)
+    // console.log(req.body)
+    let imgNames = []
+    for(let one of req.files){
+        imgNames.push(one.filename)
+    }
+    res.send(JSON.stringify(imgNames))
 })
 
 app.listen(port,(err)=>{
